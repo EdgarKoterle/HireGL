5ee809e3afa6d3b8f71881c381312b690b94543d
EdgarKoterle <EdgarKoterle@akademiasovy.sk>
2018-02-06 13:28:15 +0100

    Added ajax

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 $(document).ready(function()
 {
-	
-	$("#errorMsg").hide();
+	$("#errorMsg").empty();
 
 	$("#loginBtn").click(function()
 	{
 		var username=$("#username").val();
 		var password=$("#password").val();
 		if (username.length<2 || password.length<2)
-			$("#errorMsg").show();
+		{
+			$("#errorMsg").text('Username and password must have at least 2 characters');
+		}
 		else
-			$("#errorMsg").hide();
+			$("#errorMsg").empty();
+
+		$.ajax
+		({
+			url: 'http://akademiasovy.ddns.net:8081/api/auth/login',
+			type: 'POST',
+			contentType: 'application/json',
+
+			data: JSON.stringify
+			({
+				"userName": username,
+				"password": password
+			}),
+
+			success: function(data)
+			{
+				window.location.href='interview.html';
+			},
+
+			error: function()
+			{
+				$("#errorMsg").empty();
+				$("#errorMsg").html('Invalid Username or Password.<br>Please try again.');
+			}
+		});
+		
 	});
 });
